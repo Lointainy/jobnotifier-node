83c045a61e207532ddb81e338058257d11c80243
Eugene Kozakov <lointainyyy@gmail.com>
2024-06-13 20:03:23 +0300

    added setup for db, refactored logic for start server

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const dotenv = require('dotenv');
 const TelegramBot = require('node-telegram-bot-api');
+const prisma = require('./prisma');
+
+const connectDB = require('./config/connectDB');
 const globals = require('./config/globals');
 const registerCommands = require('./bot/botCommands');
 const registerCallbacks = require('./bot/botCallbacks');
@@ -9,5 +12,19 @@ dotenv.config();
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
-registerCommands(bot);
-registerCallbacks(bot);
+const startServer = async () => {
+	try {
+		await connectDB();
+		await prisma.$connect();
+
+		console.log('Prisma connected successfully!');
+
+		registerCommands(bot);
+		registerCallbacks(bot);
+	} catch (error) {
+		console.error('Error starting server:', error);
+		process.exit(1);
+	}
+};
+
+startServer();
